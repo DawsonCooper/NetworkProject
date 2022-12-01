dd641ff729503f5492c61d9412f9f6cdc684a310
Dawson Cooper <83229549+DawsonCooper@users.noreply.github.com>
2022-12-01 11:17:28 -0600

    Create new models for post, likes, comments and added to default user model

--- network/models.py
+++ network/models.py
@@ -3,4 +3,25 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    first_name = models.CharField(max_length=24)
+    bio = models.CharField(max_length=150)
+
+
+class Post(models.Model):
+    userId = models.ForeignKey(User, default=None)
+    caption = models.CharField(max_length=300)
+    image = models.ImageField(default=None)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    totalLikes = models.IntegerField(default=0)
+    totalComments = models.IntegerField(default=0)
+
+
+class Likes(models.Model):
+    userId = models.ForeignKey(User, default=None)
+    postId = models.ForeignKey(Post, default=None)
+
+
+class Comment(models.Model):
+    userId = models.ForeignKey(User, default=None)
+    postId = models.ForeignKey(Post, default=None)
+    content = models.CharField(max_length=150)
